dca71066b984308122834d2446718f6e34c8fa6c
Cyril Ferlicot <cyril.ferlicot@gmail.com>
2015-05-05 11:29:22 +0200

    compile.sh updated to work with the last version of Pillar

--- compile.sh
+++ compile.sh
@@ -30,12 +30,13 @@ function pillar_one() {
 
 function mypdflatex() {
   pillar_file="$1"
+  basename=${pillar_file%.*}
 
   echo "Compiling PDF from $pillar_file..."
-  ${LATEX_COMPILER} -halt-on-error -file-line-error -interaction batchmode "$pillar_file" 2>&1 1>/dev/null
+  ${LATEX_COMPILER} -halt-on-error -file-line-error -interaction batchmode "$basename" 2>&1 1>/dev/null
   ret=$?
   if [[ $ret -ne 0 ]]; then
-    cat $pillar_file.log
+    cat $basename.log
     echo "Can't generate the PDF!"
     exit 1
   fi
