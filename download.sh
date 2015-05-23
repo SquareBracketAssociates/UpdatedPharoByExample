5f9b56ed3169ec1c358ff75fcff28c9704d1acaf
jecisc <cyril.ferlicot@etudiant.univ-lille1.fr>
2015-05-23 18:43:28 +0200

    updated the script to get Pharo 4 instead of Pharo 3

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VM_INSTALL_URL="http://get.pharo.org/vm"
-IMAGE_URL="https://ci.inria.fr/pharo-contribution/job/Pillar/PHARO=30,VERSION=stable,VM=vm/lastSuccessfulBuild/artifact/Pillar.zip"
+IMAGE_URL="https://ci.inria.fr/pharo-contribution/job/Pillar/PHARO=40,VERSION=stable,VM=vm/lastSuccessfulBuild/artifact/Pillar.zip"
 
 usage() {
     cat <<HELP
